db274c844d54b42a3e7d3653d5af91f2a0d3a1a5
GEMUEL CORNILLEZ <gemueldajaycornillez152003@gmail.com>
2025-03-02 21:28:41 +0800

    Finished with minor bugs
    - Fix html format for the seconds
    - fix the css

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopwatch',
+  imports: [CommonModule],
+  templateUrl: './stopwatch.component.html',
+  styleUrl: './stopwatch.component.scss'
+})
+export class StopwatchComponent {
+  elapsedTime = 0;
+  isRunning = false;
+  intervalRef: any;
+
+  startStop() {
+    this.isRunning ? this.stop() :this.start();
+  }
+
+    private start() {
+      this.isRunning = true;
+      this.intervalRef = setInterval(() => {
+        this.elapsedTime += 0.1;
+      },100)
+    }
+
+    private stop() {
+      this.isRunning = false;
+      clearInterval(this.intervalRef);
+    }
+
+    reset() {
+      this.isRunning = false;
+      clearInterval(this.intervalRef);
+      this.elapsedTime = 0;
+
+    }
+}
